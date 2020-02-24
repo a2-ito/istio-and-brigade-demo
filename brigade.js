89d811c3c533f3a5533b24960c152895014c03f8
a2-ito <akihiko@SMS06788.asggroup.com.au>
2020-02-24 17:01:48 +1100

    update

--- brigade.js
+++ brigade.js
@@ -17,63 +17,69 @@ events.on("push", (brigadeEvent, project) => {
   //brigConfig.set("apiDHBImage", `${brigConfig.get("dhubServer")}/${brigConfig.get("apiImage")}`)
 
   var docker = new Job("job-runner-docker")
+  var helm = new Job("job-runner-helm")
   //docker.serviceAccount = "tiller"
   console.log("imageTag: ", `${brigConfig.get("imageTag")}`)
-	dockerJobRunner(brigConfig, docker, "test")
+	dockerJobRunner(brigConfig, docker)
+  helmJobRunner(brigConfig, helm, 100, 0, "prod")
 
   var pipeline = new Group()
   pipeline.add(docker)
+  pipeline.add(helm)
   if (brigConfig.get("branch") == "master") {
     pipeline.runEach()
   } else {
     console.log(`==> no jobs to run when not master`)
   }  
+
+})
+
+events.on("pull_request", (brigadeEvent, project) => {
+
+  var brigConfig = new Map()
+	brigConfig.set("apiImage", "a2ito/smackapi")
+	brigConfig.set("gitSHA", brigadeEvent.revision.commit.substr(0,7))
+	brigConfig.set("eventType", brigadeEvent.type)
+  brigConfig.set("branch", brigadeEvent.revision.ref.split('/').pop())
+	brigConfig.set("imageTag", `${brigConfig.get("branch")}-${brigConfig.get("gitSHA")}`)
+  brigConfig.set("apiDHBImage", `${brigConfig.get("apiImage")}`)
+
+  var docker = new Job("job-runner-docker")
+  var helm = new Job("job-runner-helm")
+	dockerJobRunner(brigConfig, docker)
+  helmJobRunner(brigConfig, helm, 10, 90, "prod")
+  //docker.serviceAccount = "tiller"
+  var pipeline = new Group()
+  pipeline.add(docker)
+  pipeline.add(helm)
+  pipeline.runEach()
 
 })
 
 events.on("exec", () => {
-  /*
-	var job = new Job("do-nothing", "alpine:3.8");
-  job.tasks = [
-    "echo Hello",
-    "echo World"
-  ];
-  job.run();
-	*/
-  console.log("Hello World!!!")
 
-  //var gitPayload = JSON.parse(brigadeEvent.payload)
   var brigConfig = new Map()
-/*
-	brigConfig.set("dhubServer", project.secrets.dhubServer)
-  brigConfig.set("dhubUsername", project.secrets.dhubUsername)
-  brigConfig.set("dhubPassword", project.secrets.dhubPassword)
-*/
-  brigConfig.set("apiImage", "a2ito/smackapi")
-	//brigConfig.set("gitSHA", brigadeEvent.commit.substr(0,7))
-  brigConfig.set("gitSHA", "hogehoge")
+	brigConfig.set("apiImage", "a2ito/smackapi")
+	//brigConfig.set("gitSHA", brigadeEvent.revision.commit.substr(0,7))
 	//brigConfig.set("eventType", brigadeEvent.type)
-  //brigConfig.set("branch", getBranch(gitPayload))
-  brigConfig.set("branch", "master")
-	brigConfig.set("imageTag", `${brigConfig.get("branch")}-${brigConfig.get("gitSHA")}`)
+  //brigConfig.set("branch", brigadeEvent.revision.ref.split('/').pop())
+	//brigConfig.set("imageTag", `${brigConfig.get("branch")}-${brigConfig.get("gitSHA")}`)
+	brigConfig.set("imageTag", "hoge-hoge")
   brigConfig.set("apiDHBImage", `${brigConfig.get("apiImage")}`)
-  //brigConfig.set("apiDHBImage", `${brigConfig.get("dhubServer")}/${brigConfig.get("apiImage")}`)
 
   var docker = new Job("job-runner-docker")
+  var helm = new Job("job-runner-helm")
+	dockerJobRunner(brigConfig, docker)
+  helmJobRunner(brigConfig, helm, 100, 0, "prod")
   //docker.serviceAccount = "tiller"
-	dockerJobRunner(brigConfig, docker, "test")
-
   var pipeline = new Group()
   pipeline.add(docker)
-  if (brigConfig.get("branch") == "master") {
-    pipeline.runEach()
-  } else {
-    console.log(`==> no jobs to run when not master`)
-  }  
+  pipeline.add(helm)
+  pipeline.runEach()
 
 })
 
-function dockerJobRunner(config, d, deployType) {
+function dockerJobRunner(config, d) {
     d.storage.enabled = false
     d.image = "lachlanevenson/k8s-helm:2.7.0"
     d.tasks = [
@@ -87,8 +93,7 @@ function helmJobRunner (config, h, prodWeight, canaryWeight, deployType) {
     h.image = "lachlanevenson/k8s-helm:2.7.0"
     h.tasks = [
         "cd /src/",
-        `helm upgrade --install smackapi-${deployType} ./charts/smackapi --namespace microsmack --set api.image=${config.get("apiDHBImage")} --set api.imageTag=${config.get("imageTag")} --set api.deployment=smackapi-${deployType} --set api.versionLabel=${deployType}`,
-        `helm upgrade --install microsmack-routes ./charts/routes --namespace microsmack --set prodLabel=prod --set prodWeight=${prodWeight} --set canaryLabel=new --set canaryWeight=${canaryWeight}`
+        `helm upgrade --install smackapi-${deployType} ./kube-con-2017-ito/charts/smackapi --namespace microsmack --set api.image=${config.get("apiDHBImage")} --set api.imageTag=${config.get("imageTag")} --set api.deployment=smackapi-${deployType} --set api.versionLabel=${deployType}`
     ]
 }
 
